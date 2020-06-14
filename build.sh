4054140ff98466a0122cce844d7846f6efde76ff
Hugo Huang <whenkek@live.com>
2020-06-14 16:49:03 -0700

    Replacing docker images with arm compatible ones
    Signed-off-by: Hugo Huang <whenkek@live.com>

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ TAG=2.4.5-hadoop2.7
 
 build() {
     NAME=$1
-    IMAGE=bde2020/spark-$NAME:$TAG
+    IMAGE=whenkek/spark-$NAME:$TAG
     cd $([ -z "$2" ] && echo "./$NAME" || echo "$2")
     echo '--------------------------' building $IMAGE in $(pwd)
     docker build -t $IMAGE .
@@ -17,6 +17,6 @@ build base
 build master
 build worker
 build submit
-build java-template template/java
-build scala-template template/scala
-build python-template template/python
+#build java-template template/java
+#build scala-template template/scala
+#build python-template template/python
